8a4643c8677015258596096e86f54501e4dea525
adnan morshed <adnan.starit118@gmail.com>
2020-11-25 18:12:51 -0800

    showing hard data items shopping page

--- lib/controllers/shpping_controller.dart
+++ lib/controllers/shpping_controller.dart
@@ -16,10 +16,10 @@ class ShoppingController extends GetxController {
   void fetchItems() {
    var itemsResponse=[
     Item(id: 1,iName:'itemName',iPrice:10,iDescription: 'itemDescription 01',),
-    Item(id: 1,iName:'itemName 2',iPrice:30,iDescription: 'itemDescription 02',),
-    Item(id: 1,iName:'itemName 3',iPrice:50,iDescription: 'itemDescription 03',),
-    Item(id: 1,iName:'itemName 4',iPrice:70,iDescription: 'itemDescription 04',),
-    Item(id: 1,iName:'itemName 5',iPrice:90,iDescription: 'itemDescription 05',),
+    Item(id: 2,iName:'itemName 2',iPrice:30,iDescription: 'itemDescription 02',),
+    Item(id: 3,iName:'itemName 3',iPrice:50,iDescription: 'itemDescription 03',),
+    Item(id: 4,iName:'itemName 4',iPrice:70,iDescription: 'itemDescription 04',),
+    Item(id: 5,iName:'itemName 5',iPrice:90,iDescription: 'itemDescription 05',),
    ];
    items.value=itemsResponse;
   }
--- lib/views/shopping_page.dart
+++ lib/views/shopping_page.dart
@@ -1,35 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getxformat/controllers/shpping_controller.dart';
 
 class ShoppingPage extends StatelessWidget {
-  final shoppingController=Get.put(ShoppingPage());
+  final shoppingController=Get.put(ShoppingController());
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
-            ListView.builder(
-                itemCount: 5,
-                itemBuilder:(context,index){
-                  return Card(
-                    child: ListTile(
+            Expanded(
+              child: GetX<ShoppingController>(
+                builder: (shopController){
+                return ListView.builder(
+                  itemCount: shopController.items.length,
+                  itemBuilder:(context,index){
+                    return Card(
 
-                      title: Text('titleName'),
-                      subtitle: Text('description'),
-                      trailing: Column(
-                        children: [
-                          Text('Price'),
-                          RaisedButton(onPressed: (){
+                      child: ListTile(
+                        leading: Text(shoppingController.items[index].id.toString()),
+                        title: Text(shopController.items[index].iName),
+                        subtitle:  Text(shopController.items[index].iPrice.toString()),
+                        trailing: RaisedButton(onPressed: (){
 
-                          },
-                          child: Text('add to cart'),)
-                        ],
+                        },
+                          child: Text('cart'),)
                       ),
-                    ),
-                  );
-                }
+                    );
+                  }
 
+              );
+                },
+              )
             ),
             Text('Total Amount :25'),
           ],
